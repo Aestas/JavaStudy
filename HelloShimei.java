11b57b263e92b50e18884af0509d6e5bb248697e
Natsu <zhoupeng950823@gmail.com>
2018-07-30 14:44:34 +0800

    Class name should be same with Java file name

---
+++ HelloShimei.java
@@ -0,0 +1,13 @@
+public class HelloShimei {   //如果类是public类型，那么文件名应该和public类名相同
+    public static void main(String[] args) {
+        System.out.println("你好师妹");
+    }
+}
+
+class A{
+
+}
+
+class B {
+
+}
